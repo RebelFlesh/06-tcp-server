ed51184c3193c4853428d0819c1a7fb389971855
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-21 09:37:14 -0500

    added dm tests in class

--- __tests__/lib/parser.test.js
+++ __tests__/lib/parser.test.js
@@ -3,4 +3,24 @@
 const { parser } = require('../../src/lib/parser');
 
 describe('parser', () => {
+  describe('@dm', () =>{
+    it('parses target nickname from line', done=>{
+      var line = "@dm keith hello";
+
+      parser(line, (event, target, message) =>{
+        expect(event).toBe('@dm');
+        expect(target).toBe('keith');
+        expect(message).toBe('hello');
+        done();
+      });
+    });
+
+    it('emits @help if missing target', done =>{
+      parser('@dm ', (event) =>{
+        expect(event).toBe('@help');
+        done();
+      })
+    });
+  })
+
 });
