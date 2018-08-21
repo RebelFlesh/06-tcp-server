d0c9679188fc5a14cdf4db135958a239becfcb6e
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-20 22:45:04 -0500

    added parse for list prompt

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -4,10 +4,9 @@ exports.parser = function(line, emitCallback) {
   if(line.startsWith('@all')){
     return emitCallback('@all',line.substring(5));
   }
-  //TODO: Actually implement
-  emitCallback('@all',line);
-  //if line starts with @all, emit@all message
-  //if line starts with @dm <name> message,
+  if(line.startsWith('@list')){
+    return emitCallback('@list',line.substring(6))
+  }
   //  emit @dm with [name, message]
   //if line starts with @nick <name>,
   //  emit @nick with [name]
